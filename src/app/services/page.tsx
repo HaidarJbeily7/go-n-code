1c33cf95a49472006b29833e755176c3dd21807f
MohamadSafi <the.mohammad.safi@gmail.com>
2024-03-10 23:30:35 +0300

    Created Services Hero section

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,9 +1,10 @@
 import Navbar from "@/components/navbar";
+import ServicesHero from "@/components/services/hero";
 
 export default function Services() {
   return (
     <div className="flex min-h-screen flex-col">
-      <Navbar />
+      <ServicesHero />
     </div>
   );
 }
---
+++ src/components/services/hero.tsx
@@ -0,0 +1,48 @@
+import { Box, Flex, Text, Button, Heading } from "@chakra-ui/react";
+import Navbar from "../navbar";
+
+export default function ServicesHero() {
+  return (
+    <div
+      className="flex min-h-screen flex-col bg-[#F8F9FC]"
+      style={{
+        background: "radial-gradient(#FFA50026, #F8F9FC)",
+      }}
+    >
+      <Navbar />
+      <Box>
+        <Flex justifyContent={"space-between"} my={4} mx={24}>
+          <Box maxW={"45%"} my={24}>
+            <Heading
+              as={"h1"}
+              fontWeight={700}
+              fontSize={"50px"}
+              lineHeight={"78px"}
+              color={"#393536"}
+              whiteSpace={"pre-line"}
+            >
+              We have a variety of{"\n"} different services, so you must find
+              what your{"\n"} <span className="text-[#FFA500]">company</span>{" "}
+              needs here
+            </Heading>
+          </Box>
+          <Box>
+            <img height={700} width={700} src="/services/services-hero.gif" />
+          </Box>
+        </Flex>
+        <Box position="relative">
+          <div
+            style={{
+              position: "absolute",
+              bottom: 1,
+              left: 40,
+              zIndex: 0,
+            }}
+          >
+            <img src={"/homepage/polish-circle.svg"} />
+          </div>
+        </Box>
+      </Box>
+    </div>
+  );
+}
